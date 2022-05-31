93b47cb035334338bf54332784eb504d0220ec7d
VivianTang11 <vivian.tang@hotmail.se>
2022-05-31 21:23:59 +0200

    Added calculation-function

--- script.js
+++ script.js
@@ -1,62 +1,59 @@
 const displayScreen = document.querySelector('.display-screen')
-const numberBtns = document.querySelectorAll('.number')
-const operatorBtns = document.querySelectorAll('.operator')
-const delBtn = document.querySelector('.del')
-const resetBtn = document.querySelector('.reset')
-const equalBtn = document.querySelector('.equal')
-const buttons = document.querySelectorAll('.buttons')
-
-
-const bodyElem = document.querySelector("body");
-let toggleBtns = document.getElementsByClassName("toggle");
+const buttons = Array.from(document.querySelectorAll('.button'))
+const bodyElem = document.querySelector('body');
+let toggleBtns = document.getElementsByClassName('toggle');
 let arr = [...toggleBtns];
 
 const toggleTheme = () => {
     arr.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        btn.style.opacity = "1";
-        arr.filter((item) => { return item != btn}).forEach((item) => { item.style.opacity = "0"});
-        document.body.addEventListener("change", (e) => {
+      btn.addEventListener('click', () => {
+        btn.style.opacity = '1';
+        arr.filter((item) => { return item != btn}).forEach((item) => { item.style.opacity = '0'});
+        document.body.addEventListener('change', (e) => {
             let target = e.target;
             switch (target.id) {
-              case "one":
-                bodyElem.setAttribute("data-theme", "one");
+              case 'one':
+                bodyElem.setAttribute('data-theme', 'one');
                 break;
-              case "two":
-                bodyElem.setAttribute("data-theme", "two");
+              case 'two':
+                bodyElem.setAttribute('data-theme', 'two');
                 break;
-              case "three":
-                bodyElem.setAttribute("data-theme", "three");
+              case 'three':
+                bodyElem.setAttribute('data-theme', 'three');
                 break;
             }
           });
-        
       });
     });
 }
 
-const clickedBtns = () => {
-    buttons.forEach(btn => {
-        btn.addEventListener('click', () => {
-            op = btn.textContent
-            display(op)
-        })
-    })
-}
-
-let text = ''
-const display = (op) => {
-    text += op
-    displayScreen.innerHTML = `${text}`
-    return text
-}
-
 const calculate = () => {
-    equalBtn.addEventListener('click', () => {
-        console.log(display(op));
-    })
+  buttons.map( button => {
+    button.addEventListener('click', (e) => {
+      switch(e.target.innerText){
+        case 'RESET':
+          displayScreen.innerText = '';
+          break;
+        case '=':
+          try{
+            displayScreen.innerText = eval(displayScreen.innerText);
+          } catch {
+            displayScreen.innerText = 'Error'
+          }
+          break;
+        case 'DEL':
+          if (displayScreen.innerText){
+            displayScreen.innerText = displayScreen.innerText.slice(0, -1);
+          }
+          break;
+        default:
+          displayScreen.innerText += e.target.innerText;
+      }
+    });
+  });
 }
 
-toggleTheme()
-clickedBtns()
-calculate()
+window.addEventListener('load', () => {
+  toggleTheme()
+  calculate()
+})
